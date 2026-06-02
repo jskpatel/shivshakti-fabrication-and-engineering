e69eeb791612fa272ea3c3798fda41f4c807ef97
SUNIL PATEL <75033742+jskpatel@users.noreply.github.com>
2026-06-02 13:25:45 +0530

    Init Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+import About from "@/components/About";
+import Services from "@/components/Services";
+import Process from "@/components/Process";
+import WhyChoose from "@/components/WhyChoose";
+import Team from "@/components/Team";
+import Testimonials from "@/components/Testimonials";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <About />
+      <Services />
+      <Process />
+      <WhyChoose />
+      <Team />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+
+  );
+}
